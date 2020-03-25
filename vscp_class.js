e254bc808a67f1c91aa06153381c3944193022fe
Ake Hedman <akhe@grodansparadis.com>
2020-03-25 17:05:39 +0100

    Added types to class.information as suggested by torky

--- vscp_class.js
+++ vscp_class.js
@@ -39,7 +39,7 @@
             !!!!!!!!!!!!!!!!!!!!  W A R N I N G  !!!!!!!!!!!!!!!!!!!!
                            This file is auto-generated
                 see https://github.com/grodansparadis/vscp-classes
-                        Generated: 2020-02-20 23:07:57.147524
+                        Generated: 2020-03-25 16:54:54.125306
 */
 
 
