8f7f8c77eca6f028ae49b9cdf0bdb679cfcf6f74
Ake Hedman <akhe@grodansparadis.com>
2020-04-26 22:58:36 +0200

    Updated class/type deinfitions

--- vscp_class.js
+++ vscp_class.js
@@ -39,7 +39,7 @@
             !!!!!!!!!!!!!!!!!!!!  W A R N I N G  !!!!!!!!!!!!!!!!!!!!
                            This file is auto-generated
                 see https://github.com/grodansparadis/vscp-classes
-                        Generated: 2020-03-25 16:54:54.125306
+                        Generated: 2020-04-24 18:04:56.401537
 */
 
 
